39609a2f4ae6e8f955230088857ef2c532e2d156
Vedant Mishra <vedantmishra050302@gmail.com>
2024-08-01 18:12:31 +0530

    hellu

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import {
   useQuery,
 } from "@tanstack/react-query";
 import styles from "./App.module.css";
-///
+
 const queryClient = new QueryClient();
 
 function renderGraph(root, data) {
   const layout = {
-    title: `Commits to the facebook/react repo (${new Date(data[0].week * 1000).toLocaleDateString()} - ${new Date(data[data.length - 1].week * 1000).toLocaleDateString()})`,
+    title: `Commits to the mdn/content repo (${new Date(data[0].week * 1000).toLocaleDateString()} - ${new Date(data[data.length - 1].week * 1000).toLocaleDateString()})`,
     xaxis: {
       title: "Week",
       showgrid: true,
@@ -32,7 +32,7 @@ function renderGraph(root, data) {
 
 function renderGrid(root, data) {
   const layout = {
-    title: `Commits to the facebook/react repo, by day`,
+    title: `Commits to the mdn/content repo, by day`,
     height: 350,
   };
   const zValues = Array.from({ length: 7 }, () => []);
@@ -55,7 +55,7 @@ function Example() {
     queryKey: ["repoData"],
     queryFn: () =>
       fetch(
-        "https://api.github.com/repos/facebook/react/stats/commit_activity",
+        "https://api.github.com/repos/mdn/content/stats/commit_activity",
       ).then((res) => res.json()),
   });
 
